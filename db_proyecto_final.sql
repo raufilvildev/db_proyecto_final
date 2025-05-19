baf6afff8e5b12e5f50efef2740e5cb0babdc18e
Raúl Filigrana Villalba <raufilvil@gmail.com>
2025-05-19 15:59:30 +0200

    Agrego columna token en la tabla user.

--- db_proyecto_final.sql
+++ db_proyecto_final.sql
@@ -5,13 +5,16 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+-- -----------------------------------------------------
 -- Schema db_proyecto_final
 -- -----------------------------------------------------
 
 -- -----------------------------------------------------
 -- Schema db_proyecto_final
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `db_proyecto_final` DEFAULT CHARACTER SET utf8 ;
+CREATE SCHEMA IF NOT EXISTS `db_proyecto_final` DEFAULT CHARACTER SET utf8mb3 ;
 USE `db_proyecto_final` ;
 
 -- -----------------------------------------------------
@@ -19,20 +22,23 @@ USE `db_proyecto_final` ;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `db_proyecto_final`.`user` (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(100) NOT NULL,
+  `name` VARCHAR(100) CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_0900_ai_ci' NOT NULL,
   `birth_date` DATE NOT NULL,
-  `gender` VARCHAR(2) NOT NULL,
-  `phone` VARCHAR(20) NULL,
-  `email` VARCHAR(320) NOT NULL,
-  `username` VARCHAR(50) NOT NULL,
-  `password` VARCHAR(200) NOT NULL,
-  `is_notified_by_email` TINYINT UNSIGNED NOT NULL DEFAULT 1,
+  `gender` VARCHAR(2) CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_0900_ai_ci' NOT NULL,
+  `phone` VARCHAR(20) CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_0900_ai_ci' NULL DEFAULT NULL,
+  `email` VARCHAR(320) CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_0900_ai_ci' NOT NULL,
+  `username` VARCHAR(50) CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_0900_ai_ci' NOT NULL,
+  `password` VARCHAR(200) CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_0900_ai_ci' NOT NULL,
+  `is_notified_by_email` TINYINT UNSIGNED NOT NULL DEFAULT '1',
+  `token` VARCHAR(6) NULL DEFAULT NULL,
   `created_at` TIMESTAMP NOT NULL,
   `updated_at` TIMESTAMP NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE,
   UNIQUE INDEX `username_UNIQUE` (`username` ASC) VISIBLE)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+AUTO_INCREMENT = 35
+DEFAULT CHARACTER SET = utf8mb3;
 
 
 -- -----------------------------------------------------
@@ -40,16 +46,15 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `db_proyecto_final`.`category` (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `title` VARCHAR(100) NULL,
+  `title` VARCHAR(100) CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_0900_ai_ci' NULL DEFAULT NULL,
   `user_id` INT UNSIGNED NOT NULL,
   PRIMARY KEY (`id`),
   INDEX `fk_category_user1_idx` (`user_id` ASC) VISIBLE,
   CONSTRAINT `fk_category_user1`
     FOREIGN KEY (`user_id`)
-    REFERENCES `db_proyecto_final`.`user` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+    REFERENCES `db_proyecto_final`.`user` (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
 
 
 -- -----------------------------------------------------
@@ -57,9 +62,9 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `db_proyecto_final`.`task` (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `description` TEXT NOT NULL,
-  `url` VARCHAR(255) NOT NULL,
-  `is_notified_by_email` TINYINT UNSIGNED NOT NULL DEFAULT 1,
+  `description` TEXT CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_0900_ai_ci' NOT NULL,
+  `url` VARCHAR(255) CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_0900_ai_ci' NOT NULL,
+  `is_notified_by_email` TINYINT UNSIGNED NOT NULL DEFAULT '1',
   `created_at` TIMESTAMP NOT NULL,
   `updated_at` TIMESTAMP NOT NULL,
   `user_id` INT UNSIGNED NOT NULL,
@@ -67,17 +72,32 @@ CREATE TABLE IF NOT EXISTS `db_proyecto_final`.`task` (
   PRIMARY KEY (`id`),
   INDEX `fk_task_user_idx` (`user_id` ASC) VISIBLE,
   INDEX `fk_task_category1_idx` (`category_id` ASC) VISIBLE,
-  CONSTRAINT `fk_task_user`
-    FOREIGN KEY (`user_id`)
-    REFERENCES `db_proyecto_final`.`user` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
   CONSTRAINT `fk_task_category1`
     FOREIGN KEY (`category_id`)
-    REFERENCES `db_proyecto_final`.`category` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+    REFERENCES `db_proyecto_final`.`category` (`id`),
+  CONSTRAINT `fk_task_user`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `db_proyecto_final`.`user` (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
+
+
+-- -----------------------------------------------------
+-- Table `db_proyecto_final`.`recurrent_task`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `db_proyecto_final`.`recurrent_task` (
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `day_of_the_week` ENUM('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday') CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_0900_ai_ci' NOT NULL,
+  `time_start` TIME NOT NULL,
+  `time_end` TIME NOT NULL,
+  `task_id` INT UNSIGNED NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_recurrent_task_task1_idx` (`task_id` ASC) VISIBLE,
+  CONSTRAINT `fk_recurrent_task_task1`
+    FOREIGN KEY (`task_id`)
+    REFERENCES `db_proyecto_final`.`task` (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
 
 
 -- -----------------------------------------------------
@@ -88,33 +108,13 @@ CREATE TABLE IF NOT EXISTS `db_proyecto_final`.`unique_task` (
   `date` DATE NOT NULL,
   `time_start` TIME NOT NULL,
   `time_end` TIME NOT NULL,
-  INDEX `fk_unique_task_task1_idx` (`task_id` ASC) VISIBLE,
   PRIMARY KEY (`task_id`),
+  INDEX `fk_unique_task_task1_idx` (`task_id` ASC) VISIBLE,
   CONSTRAINT `fk_unique_task_task1`
     FOREIGN KEY (`task_id`)
-    REFERENCES `db_proyecto_final`.`task` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `db_proyecto_final`.`recurrent_task`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `db_proyecto_final`.`recurrent_task` (
-  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `day_of_the_week` ENUM('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday') NOT NULL,
-  `time_start` TIME NOT NULL,
-  `time_end` TIME NOT NULL,
-  `task_id` INT UNSIGNED NOT NULL,
-  INDEX `fk_recurrent_task_task1_idx` (`task_id` ASC) VISIBLE,
-  PRIMARY KEY (`id`),
-  CONSTRAINT `fk_recurrent_task_task1`
-    FOREIGN KEY (`task_id`)
-    REFERENCES `db_proyecto_final`.`task` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+    REFERENCES `db_proyecto_final`.`task` (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
